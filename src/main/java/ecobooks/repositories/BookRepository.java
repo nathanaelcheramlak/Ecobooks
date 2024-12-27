066bc2cbac5baaa56ae231f1a59a4d472cf30e90
nathanaelcheramlak <nathanaelcheramlak7@gmail.com>
2024-12-27 23:09:22 +0300

    search books using keyword added

--- src/main/java/ecobooks/repositories/BookRepository.java
+++ src/main/java/ecobooks/repositories/BookRepository.java
@@ -3,6 +3,7 @@ package ecobooks.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import ecobooks.models.BookModel;
@@ -14,4 +15,10 @@ public interface BookRepository extends JpaRepository<BookModel, Long>{
     List<BookModel> findByAuthor(String author);
     List<BookModel> findByGenre(String genre);
     List<BookModel> findBySeller(UserModel seller);
+
+    @Query("SELECT b FROM BookModel b WHERE " +
+           "LOWER(b.title) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+           "LOWER(b.author) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+           "LOWER(b.genre) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<BookModel> searchBooks(String keyword);
 }
