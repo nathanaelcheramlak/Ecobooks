7f216904bcce4cc673361d98c21e3fc942e83448
nathanaelcheramlak <nathanaelcheramlak7@gmail.com>
2024-12-30 01:25:09 +0300

    query now works if one word matches

--- src/main/java/ecobooks/repositories/BookRepository.java
+++ src/main/java/ecobooks/repositories/BookRepository.java
@@ -11,9 +11,15 @@ import ecobooks.models.UserModel;
 
 @Repository
 public interface BookRepository extends JpaRepository<BookModel, Long>{
+    @Query("SELECT b FROM BookModel b WHERE LOWER(b.title) LIKE LOWER(CONCAT('%', :title, '%'))")
     List<BookModel> findByTitle(String title);
+
+    @Query("SELECT b FROM BookModel b WHERE LOWER(b.author) LIKE LOWER(CONCAT('%', :author, '%'))")
     List<BookModel> findByAuthor(String author);
+
+    @Query("SELECT b FROM BookModel b WHERE LOWER(b.genre) LIKE LOWER(CONCAT('%', :genre, '%'))")
     List<BookModel> findByGenre(String genre);
+    
     List<BookModel> findBySeller(UserModel seller);
 
     @Query("SELECT b FROM BookModel b WHERE " +
